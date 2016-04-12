42fa5db2280fb605c065f3e7b4f83084a5124f97
medalhkr <medalhkr@gmail.com>
2016-04-12 18:05:00 +0900

    [add] projectのバリデーションを追加

--- portfolio/app/models/project.rb
+++ portfolio/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, presence: true
+  validates :url, length: { maximum: 255 }
 end
