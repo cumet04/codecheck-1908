c992480537df3bc426dffd74c50b106006115c87
medalhkr <medalhkr@gmail.com>
2016-04-13 10:35:47 +0900

    [change] projectsのパス設定とcontrollerの関数名を変更
    json-apiの仕様をそのままにweb-viewを実装するため

--- portfolio/app/controllers/projects_controller.rb
+++ portfolio/app/controllers/projects_controller.rb
@@ -1,25 +1,41 @@
 class ProjectsController < ApplicationController
-  def index
-    render json: Project.all
-  end
 
+# web-view functions
   def new
   end
 
+  def index
+  end
+
   def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+# API functions
+  def api_index
+    render json: Project.all
+  end
+
+  def api_show
     project = Project.find_by(id: params[:id])
     return head :not_found unless project
     return render json: project
   end
 
-  def create
+  def api_create
     project = Project.new(project_params)
     return head :bad_request           unless project.valid?
     return head :internal_server_error unless project.save
     return render json: project
   end
 
-  def destroy
+  def api_destroy
     project = Project.find_by(id: params[:id])
     return head :not_found unless project
 
--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'application#hello'
 
-  resources :projects, :path => 'api/projects'
+  # for web-view
+  resources :projects
+
+  # for api
+  get    'api/projects'     => 'projects#api_index'
+  post   'api/projects'     => 'projects#api_create'
+  get    'api/projects/:id' => 'projects#api_show'
+  delete 'api/projects/:id' => 'projects#api_destroy'
 end
