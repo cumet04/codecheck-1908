bcd32eb34752c676316be3469acb492c520ac603
medalhkr <medalhkr@gmail.com>
2016-04-14 11:37:47 +0900

    [add] projectのshow/index画面にthumbを表示する機能を追加

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -2,16 +2,11 @@ Rails.application.routes.draw do
 
   root 'application#hello'
 
-  # for thumb
-  get    'api/thumb'     => 'thumb#index'
-  post   'api/thumb'     => 'thumb#create'
-  get    'api/thumb/:id' => 'thumb#show'
-  delete 'api/thumb/:id' => 'thumb#destroy'
+  resources :thumb, :path => 'api/thumbs',
+            only: [:index, :create, :show, :destroy]
 
-  # for web-view
   resources :projects
 
-  # for api
   get    'api/projects'     => 'projects#api_index'
   post   'api/projects'     => 'projects#api_create'
   get    'api/projects/:id' => 'projects#api_show'
