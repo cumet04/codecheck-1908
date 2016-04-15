cc36374397242dcf5b1b740a2175c185cfc31c1e
medalhkr <medalhkr@gmail.com>
2016-04-15 15:28:38 +0900

    [fix] migrateのファイルが実が問題あったのを修正

--- portfolio/db/migrate/20160415060923_add_column_to_project.rb
+++ portfolio/db/migrate/20160415060923_add_column_to_project.rb
@@ -1,5 +1,6 @@
 class AddColumnToProject < ActiveRecord::Migration
   def change
+    add_column :projects, :thumb_id, :int
     add_column :projects, :link_count, :int
   end
 end
--- portfolio/db/schema.rb
+++ portfolio/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415035518) do
+ActiveRecord::Schema.define(version: 20160415060923) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "url"
